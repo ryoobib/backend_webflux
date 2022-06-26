c1586a41cf9c84e5a30cf83185de41bb62a7f233
dldmsql <yhy5913@naver.com>
2022-06-26 20:18:24 +0900

    chore: Setting for docker and mongodb

---
+++ src/test/java/com/example/backend_webflux/BackendWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebfluxApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
