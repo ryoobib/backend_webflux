f0160fd5ddf116bf479d582b948fbef57667d433
dldmsql <yhy5913@naver.com>
2022-07-09 17:24:12 +0900

    feat: Add dto for post domain

---
+++ src/main/java/com/example/backend_webflux/dto/PostDto.java
@@ -0,0 +1,17 @@
+package com.example.backend_webflux.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+
+    private String title;
+    private String content;
+
+}
