41eca2a939bf408b8115cbc98e0df87c905b4ea7
dldmsql <yhy5913@naver.com>
2022-07-14 15:04:43 +0900

    feat: Add reponse entity for exception

--- src/main/java/exception/ExceptionResponse.java
+++ src/main/java/exception/ExceptionResponse.java
@@ -1,2 +1,22 @@
-package exception;public class ExceptionResponse {
+package exception;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class ExceptionResponse {
+  @JsonProperty("timestamp")  private Instant timestamp;
+  @JsonProperty("path")       private String path;
+  @JsonProperty("status")     private Integer status;
+  @JsonProperty("error")      private String error;
+  @JsonProperty("message")    private String message;
+  @JsonProperty("requestId")  private String requestId;
+  @JsonProperty("exception")  private String exception;
 }
