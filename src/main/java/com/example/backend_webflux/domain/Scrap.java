a2b283c964bc562d9ca234b6f1a6bee7b7b4bc79
dldmsql <yhy5913@naver.com>
2022-07-16 18:59:51 +0900

    refactor: Edit field annoation and add fk field

--- src/main/java/com/example/backend_webflux/domain/Scrap.java
+++ src/main/java/com/example/backend_webflux/domain/Scrap.java
@@ -1,18 +1,32 @@
 package com.example.backend_webflux.domain;
 
 import java.sql.Timestamp;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Document
 public class Scrap {
 
   @Id
-  private Integer id;
+  private String id;
   private Integer status;
 
+  private String postId;
+  private String userId;
+
   @CreatedDate
-  private Timestamp createdAt;
+  private Date createdAt;
 
 }
