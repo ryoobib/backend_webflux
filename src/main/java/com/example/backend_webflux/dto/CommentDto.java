0981ea602ce20ee672db6944f91202b28a43c9f8
dldmsql <yhy5913@naver.com>
2022-07-29 14:18:05 +0900

    feat: Add Comment CRUD Api

--- src/main/java/com/example/backend_webflux/dto/CommentDto.java
+++ src/main/java/com/example/backend_webflux/dto/CommentDto.java
@@ -1,2 +1,11 @@
-package com.example.backend_webflux.dto;public class CommentDto {
+package com.example.backend_webflux.dto;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+
+  private String content;
+  private String postId;
+  private String userId;
 }
--- src/main/java/com/example/backend_webflux/repository/CommentRepository.java
+++ src/main/java/com/example/backend_webflux/repository/CommentRepository.java
@@ -1,2 +1,8 @@
-package com.example.backend_webflux.repository;public interface CommentRepository {
+package com.example.backend_webflux.repository;
+
+import com.example.backend_webflux.domain.Comment;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface CommentRepository extends ReactiveMongoRepository<Comment, String> {
+
 }
