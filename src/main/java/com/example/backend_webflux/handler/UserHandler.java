12db8adf763904c196e6f8686a87bd80a0fd1276
dldmsql <yhy5913@naver.com>
2022-08-02 16:04:17 +0900

    refactor: Seperate login handler and service and add exception handling

--- src/main/java/com/example/backend_webflux/handler/UserHandler.java
+++ src/main/java/com/example/backend_webflux/handler/UserHandler.java
@@ -4,30 +4,27 @@ import com.example.backend_webflux.domain.User;
 import com.example.backend_webflux.exception.ApiException;
 import com.example.backend_webflux.exception.ApiExceptionEnum;
 import com.example.backend_webflux.repository.UserRepository;
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.http.HttpStatus;
+import com.example.backend_webflux.service.UserService;
+import com.mongodb.internal.connection.Server;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import org.webjars.NotFoundException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
 public class UserHandler {
 
-  private final UserRepository userRepository;
+  private final UserService userService;
 
-  public UserHandler(UserRepository userRepository) {
-    this.userRepository = userRepository;
+  public UserHandler(UserService userService) {
+    this.userService = userService;
   }
 
 
   public Mono<ServerResponse> getAllUser(ServerRequest request) {
-    Flux<User> users = userRepository.findAll()
-        .switchIfEmpty(Mono.error(new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION)));
+    Flux<User> users = userService.getAllUser();
 
     return ServerResponse.ok()
         .contentType(MediaType.APPLICATION_JSON)
@@ -37,8 +34,7 @@ public class UserHandler {
   public Mono<ServerResponse> getUser(ServerRequest request) {
     String id = request.pathVariable("id");
 
-    Mono<User> user = userRepository.findById(id)
-        .switchIfEmpty(Mono.error(new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION)));
+    Mono<User> user = userService.getUserById(id);
 
     return ServerResponse.ok()
         .contentType(MediaType.APPLICATION_JSON)
@@ -48,36 +44,43 @@ public class UserHandler {
   public Mono<ServerResponse> createUser(ServerRequest request) {
     Mono<User> unsavedUser = request.bodyToMono(User.class);
 
-    Mono<User> saved = unsavedUser
-        .flatMap(userRepository::save);
+    Mono<User> saved = unsavedUser.flatMap(userService::create);
 
-    return ServerResponse.accepted()
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(saved, User.class);
+    return saved.flatMap(data -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(data))
+        .switchIfEmpty(ServerResponse.notFound().build())
+        .onErrorResume(error -> {
+          if(error instanceof ApiException) {
+            return ServerResponse.status(((ApiException) error).getError().getStatus()).bodyValue(((ApiException) error).getError().getMessage());
+          }
+          return ServerResponse.status(500).build();
+        });
   }
 
   public Mono<ServerResponse> modifyUser(ServerRequest request) {
     Mono<User> unsavedUser = request.bodyToMono(User.class);
     String id = request.pathVariable("id");
 
-    Mono<User> updatedUser = unsavedUser.flatMap(user ->
-        userRepository.findById(id)
-            .flatMap(oldUser -> {
-              user.setId(id);
-              return userRepository.save(user);
-            }));
+    Mono<User> updatedUser = unsavedUser.flatMap(u -> userService.modify(id, u));
 
-    return  ServerResponse.accepted()
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(updatedUser, User.class);
+    return updatedUser.flatMap(data -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(data))
+        .onErrorResume(error -> {
+          if(error instanceof ApiException) {
+            return ServerResponse.status(((ApiException) error).getError().getStatus()).bodyValue(((ApiException) error).getError().getMessage());
+          }
+          return ServerResponse.status(500).build();
+        });
   }
 
   public Mono<ServerResponse> deleteUser(ServerRequest request) {
     String id = request.pathVariable("id");
-    Mono<Void> deleted = userRepository.deleteById(id);
+    Mono<Void> deleted = userService.delete(id);
 
-    return ServerResponse.ok()
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(deleted, Void.class);
+    return deleted.flatMap(data -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).build())
+        .onErrorResume(error -> {
+          if(error instanceof ApiException) {
+            return ServerResponse.status(((ApiException) error).getError().getStatus()).bodyValue(((ApiException) error).getError().getMessage());
+          }
+          return ServerResponse.status(500).build();
+        });
   }
 }
--- src/main/java/com/example/backend_webflux/service/UserService.java
+++ src/main/java/com/example/backend_webflux/service/UserService.java
@@ -1,2 +1,57 @@
-package com.example.backend_webflux.service;public class UserService {
+package com.example.backend_webflux.service;
+
+import com.example.backend_webflux.domain.User;
+import com.example.backend_webflux.exception.ApiException;
+import com.example.backend_webflux.exception.ApiExceptionEnum;
+import com.example.backend_webflux.repository.UserRepository;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+public class UserService {
+
+  private final UserRepository userRepository;
+
+
+  public Mono<User> create(User user) {
+    return userRepository.findByName(user.getName())
+        .defaultIfEmpty(user)
+        .flatMap(result -> {
+          if (result.getId() == null) {
+            return userRepository.save(result);
+          } else {
+            return Mono.error(new ApiException(ApiExceptionEnum.DUPLICATION_VALUE_EXCEPTION));
+          }
+        });
+  }
+
+  public Mono<User> getUserById(String id) {
+    return userRepository.findById(id)
+        .switchIfEmpty(Mono.error(new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION)));
+  }
+
+  public Flux<User> getAllUser() {
+    return userRepository.findAll()
+        .switchIfEmpty(Mono.error(new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION)));
+  }
+
+  public Mono<User> modify(String id, User user) {
+    return userRepository.findById(id)
+        .switchIfEmpty(Mono.error(new ApiException(ApiExceptionEnum.NOT_FOUND_EXCEPTION)))
+        .flatMap(u -> {
+              u.setName(user.getName());
+              return userRepository.save(u);
+            })
+        ;
+
+  }
+
+  public Mono<Void> delete(String id) {
+    return userRepository.deleteById(id);
+  }
 }
